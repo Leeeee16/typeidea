1466b34dd6787005e5d35262ef9ff9851997e447
lqy <lele163163@163.com>
2019-08-01 11:11:42 +0800

    完成view部分的编写

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,39 +1,82 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Post, Tag, Category
 from config.models import SideBar
 
 
-def post_list(request, category_id=None, tag_id=None):
-    tag = None
-    category = None
+class CommonViewMixin:
+    """用来处理通用数据"""
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'sidebars': self.get_siderbars(),
+        })
+        context.update(self.get_navs())
+        return context
 
-    if tag_id:
-        post_list, tag = Post.get_by_tag(tag_id)
-    elif category_id:
-        post_list, category = Post.get_by_category(category_id)
-    else:
-        post_list = Post.latest_posts(Post)
+    def get_siderbars(self):
+        return SideBar.objects.filter(status=SideBar.STATUS_SHOW)
 
-    context = {
-        'category': category,
-        'tag': tag,
-        'post_list': post_list,
-        'sidebars': SideBar.get_all(),
-    }
-    context.update(Category.get_navs(Category))
-    return render(request, 'blog/list.html', context=context)
+    def get_navs(self):
+        categories = Category.objects.filter(status=Category.STATUS_NORMAL)
+        nav_categories = []
+        normal_categories = []
+        for cate in categories:
+            if cate.is_nav:
+                nav_categories.append(cate)
+            else:
+                normal_categories.append(cate)
+
+        return {
+            'nav': nav_categories,
+            'categories': normal_categories,
+        }
 
 
-def post_detail(request, post_id=None):
-    try:
-        post = Post.objects.get(id=post_id)
-    except Post.DoesNotExist:
-        post = None
+class IndexView(ListView):
+    queryset = Post.objects.filter(status=Post.STATUS_NORMAL)
+    paginate_by = 5  # 每页的数量
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'
 
-    context = {
-        'post': post,
-        'sideBars': SideBar.get_all(),
-    }
-    context.update(Category.get_navs(Category))
-    return render(request, 'blog/detail.html', context=context)
+
+class CategoryView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category, pk=category_id)
+        context.update({
+            'category': category,
+        })
+        return context
+
+    def get_queryset(self):
+        """重写queryset，根据分类过滤"""
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update({
+            'tag': tag,
+        })
+        return context
+
+    def get_queryset(self):
+        """重写queryset，根据标签过滤"""
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag__id=tag_id)
+
+
+class PostDetailView(CommonViewMixin, DetailView):
+    queryset = Post.objects.filter(status=Post.STATUS_NORMAL)
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
